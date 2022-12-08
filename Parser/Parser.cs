3e45c8c94a871338dace1f2557f7df60410b9d45
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2022-12-09 01:12:23 +0200

    Create Parser class

---
+++ Parser/Parser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    internal class Parser
+    {
+        private Node? parsingTree;
+        private Scanner scanner;
+
+        public Parser(String tinyCode)
+        {
+            this.scanner = new Scanner(tinyCode);
+        }
+
+        private Token? peek()
+        {
+            Token? top = scanner.GetNextToken();
+            scanner.GoBackTokens(1);
+            return top;
+        }
+        private bool match(Token token)
+        {
+            return scanner.GetNextToken() == token;
+        }
+        private void unmatch(Token token)
+        {
+            scanner.GoBackTokens(1);
+        }
+    }
+}
