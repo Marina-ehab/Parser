6f0412376ef6deab87deb3d9384be8bd4222789b
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2022-12-09 00:02:32 +0200

    Add methods for Node class

--- Parser/Node.cs
+++ Parser/Node.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace Parser
 {
+
     internal class Node
     {
-        string tokenType;
-        string value;
-        List<Node> children;
-        Node? nextNode;
+        private Token token;
+        private List<Node> children;
+        private Node? nextNode;
+
+        public Node(Token token)
+        {
+            this.token = token;
+            this.children = new List<Node>();
+            this.nextNode = null;
+        }
+
+        public void AddChild(Node child)
+        {
+            this.children.Add(child);
+        }
+
+        public void AddNextNode(Node nextNode)
+        {
+            this.nextNode = nextNode;
+        }
+
+        public Token GetToken()
+        {
+            return this.token;
+        }
+
     }
 }
