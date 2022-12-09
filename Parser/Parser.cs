f1b13af8cc603746e7fa773a9e3688d1cf992716
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2022-12-09 23:16:53 +0200

    Add Parser class, w/ program, stmt_sequence, and statement functions

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -8,12 +8,14 @@ namespace Parser
 {
     internal class Parser
     {
-        private Node? parsingTree;
+        public Node? parseTree;
         private Scanner scanner;
 
         public Parser(String tinyCode)
         {
             this.scanner = new Scanner(tinyCode);
+            // the final parse tree
+            parseTree = program();
         }
 
         private Token? peek()
@@ -22,13 +24,74 @@ namespace Parser
             scanner.GoBackTokens(1);
             return top;
         }
-        private bool match(Token token)
+        private void match()
         {
-            return scanner.GetNextToken() == token;
+            scanner.GetNextToken();
         }
-        private void unmatch(Token token)
+
+        private Node? program()
         {
-            scanner.GoBackTokens(1);
+            Node program = new Node(NodeType.Program);
+            program.AddChild(stmt_sequence());
+            return program;
         }
+        private Node stmt_sequence()
+        {
+            Node stmt_sequence = new Node(NodeType.StmtSequence);
+            stmt_sequence.AddChild(statement());
+            while (peek()?.tokenValue == ";")
+            {
+                match();
+                stmt_sequence.AddChild(new Node(value:";"));
+                stmt_sequence.AddChild(statement());
+            }
+            return stmt_sequence;
+        }
+
+        private Node statement()
+        {
+
+            Node statement = new Node(NodeType.Statement);
+            var top = peek();
+            switch (top?.tokenValue)
+            {
+                case "if":
+                    statement.AddChild(if_stmt());
+                    break;
+                case "repeat":
+                    statement.AddChild(repeat_stmt());
+                    break;
+                case "read":
+                    statement.AddChild(read_stmt());
+                    break;
+                case "write":
+                    statement.AddChild(write_stmt());
+                    break;
+                default:
+                    switch (top?.tokenType)
+                    {
+                        case TokenType.Identifier:
+                            statement.AddChild(assign_stmt());
+                            break;
+                        default:
+                            throw new Exception("Error at line " + top?.line + " near column " + top?.column + ". Colon without equal sign");
+                    }
+                    break;
+            }
+            return statement;
+        }
+
+        private Node assign_stmt();
+        private Node write_stmt();
+        private Node read_stmt();
+        private Node repeat_stmt();
+        private Node if_stmt();
+        private Node exp();
+        private Node term();
+        private Node simple_exp();
+        private Node factor();
+        private Node mulop();
+        private Node addop();
+        private Node comparisonop();
     }
 }
