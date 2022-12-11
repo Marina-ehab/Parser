bd826bc9bc6baf8832b9b62c38a20c7ac3580095
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2022-12-12 00:21:25 +0200

    Implement CompariosnOp, and Expression rules

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -42,7 +42,7 @@ namespace Parser
             while (peek()?.tokenValue == ";")
             {
                 match();
-                stmt_sequence.AddChild(new Node(value:";"));
+                stmt_sequence.AddChild(new Node(value: ";"));
                 stmt_sequence.AddChild(statement());
             }
             return stmt_sequence;
@@ -52,28 +52,36 @@ namespace Parser
         {
 
             Node statement = new Node(NodeType.Statement);
+
+            // stmt -> if_stmt | repeat_stmt | read_stmt | write_stmt | assign_stmt
             var top = peek();
             switch (top?.tokenValue)
             {
                 case "if":
+                    // stmt -> if_stmt
                     statement.AddChild(if_stmt());
                     break;
                 case "repeat":
+                    // stmt -> repeat_stmt
                     statement.AddChild(repeat_stmt());
                     break;
                 case "read":
+                    // stmt -> read_stmt
                     statement.AddChild(read_stmt());
                     break;
                 case "write":
+                    // stmt -> write_stmt
                     statement.AddChild(write_stmt());
                     break;
                 default:
                     switch (top?.tokenType)
                     {
                         case TokenType.Identifier:
+                            // stmt -> assign_stmt
                             statement.AddChild(assign_stmt());
                             break;
                         default:
+                            // an Exception is thrown when there's a syntax error 
                             throw new Exception("Error at line " + top?.line + " near column " + top?.column + ". Colon without equal sign");
                     }
                     break;
@@ -86,12 +94,53 @@ namespace Parser
         private Node read_stmt();
         private Node repeat_stmt();
         private Node if_stmt();
-        private Node exp();
+        private Node exp()
+        {
+            Node exp = new Node(NodeType.Expression);
+            // exp -> simple_exp
+            exp.AddChild(simple_exp());
+            
+            // exp -> simple_exp [comparisonOp simple_exp]
+            var top = peek();
+            if (top?.tokenValue == "<" || top?.tokenValue == "=")
+            {   
+                // we can make the implementation of comparisonOp here
+                // but to keep up with the convention, It's implemented in a dedicated function
+                exp.AddChild(comparisonop());
+                exp.AddChild(simple_exp());
+            }
+                
+            return exp;
+        }
+
         private Node term();
         private Node simple_exp();
         private Node factor();
         private Node mulop();
         private Node addop();
-        private Node comparisonop();
+        
+        private Node comparisonop()
+        {
+            Node comparisonop = new Node(NodeType.ComparisonOp);
+            var top = peek();
+
+            // ComparisonOp -> < | =
+            switch(top?.tokenValue)
+            {
+                case ">":
+                    comparisonop.AddChild(new Node(value: ">"));
+                    match();
+                    break;
+                case "=":
+                    comparisonop.AddChild(new Node(value: "="));
+                    match();
+                    break;
+                default:
+                    // actually this line is impossible to reach, as we checked on the top in exp() before calling compariosnop()
+                    // but you know IT'S THE LAW
+                    throw new Exception("Error at line " + top?.line + " near column " + top?.column + ". Colon without equal sign");
+            }
+            return comparisonop;
+        }
     }
 }
