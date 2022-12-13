752d63b8465f846414b967b222b37d4bd6d14db9
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2022-12-13 21:21:31 +0200

    intial ui screen

--- Parser/Form1.Designer.cs
+++ Parser/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.generate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(938, 397);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "input";
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(838, 532);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(112, 34);
+            this.generate.TabIndex = 3;
+            this.generate.Text = "generate";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 448);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "errors";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(930, 37);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(20, 397);
+            this.vScrollBar1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 532);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(975, 578);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button generate;
+        private Label label2;
+        private VScrollBar vScrollBar1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- Parser/Form1.cs
+++ Parser/Form1.cs
@@ -2,9 +2,47 @@ namespace Parser
 {
     public partial class Form1 : Form
     {
+        string text = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "tiny language program file";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "Text|*.txt";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                text = File.ReadAllText(fdlg.FileName);
+                textBox1.Text = text;
+            }
+            else
+            {
+                //error
+            }
+             
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
